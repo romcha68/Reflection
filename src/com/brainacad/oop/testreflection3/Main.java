2b0becbc9dbda4dd806ed86883fbc09e71abffcb
romcha68 <romcha68@ukr.net>
2018-04-13 12:48:05 +0300

    Solution 3 of the problem

---
+++ src/com/brainacad/oop/testreflection3/Main.java
@@ -0,0 +1,107 @@
+package com.brainacad.oop.testreflection3;
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class c = Class.forName(MyClass.class.getCanonicalName());
+
+        Class[] singleIntParam = new Class[]{int.class};
+        Constructor myClassContructor = c.getConstructor(singleIntParam);
+        MyClass instance = (MyClass) myClassContructor.newInstance(9);
+
+        c.getMethod("setA", singleIntParam).invoke(instance, 8);
+
+        System.out.println("ClassName: " + instance.getClass().getCanonicalName());
+        System.out.println("\tModifiers: \n\t\t" + getModifiersName(instance.getClass().getModifiers()));
+
+        System.out.println("\tFields: ");
+        System.out.println(Arrays.stream(instance.getClass().getDeclaredFields())
+                .map(field -> {
+                    StringBuilder fieldDeclaration = new StringBuilder("");
+                    fieldDeclaration.append("\t\tField:\n");
+                    fieldDeclaration.append("\t\t\tName: " + field.getName() + "\n");
+                    fieldDeclaration.append("\t\t\tModifiers: " + getModifiersName(field.getModifiers()) + "\n");
+                    fieldDeclaration.append("\t\t\tType: " + field.getType() + "\n");
+                    field.setAccessible(true);
+                    try {
+                        fieldDeclaration.append("\t\t\tValue: " + field.get(instance));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+
+                    return fieldDeclaration.toString();
+                }).collect(Collectors.joining("\n")));
+
+        System.out.println("\tConstructors: ");
+        Arrays.stream(instance.getClass().getDeclaredConstructors())
+                .forEach(constructor ->
+                        System.out.println("\t\tConstructor: " + Arrays.stream(constructor.getParameterTypes())
+                                .map(field -> field.getName())
+                                .collect(Collectors.joining(", "))));
+
+        System.out.println("\tMethods: ");
+        System.out.println(Arrays.stream(instance.getClass().getDeclaredMethods())
+                .map(method -> {
+                    StringBuilder methodDeclaration = new StringBuilder("");
+                    methodDeclaration.append("\t\tMethod:\n");
+                    methodDeclaration.append("\t\t\tName: " + method.getName() + "\n");
+                    methodDeclaration.append("\t\t\tModifiers: " + getModifiersName(method.getModifiers()) + "\n");
+                    methodDeclaration.append("\t\t\tReturn type: " + method.getReturnType().getName() + "\n");
+                    methodDeclaration.append("\t\t\tParam types: " + Arrays.stream(method.getParameterTypes())
+                            .map(parameterType -> parameterType.getName())
+                            .collect(Collectors.joining(", ")));
+
+                    return methodDeclaration.toString();
+                }).collect(Collectors.joining("\n")));
+    }
+
+    private static String getModifiersName(final int modifiersNumber) {
+        StringBuilder name = new StringBuilder("");
+        if (Modifier.isAbstract(modifiersNumber))
+            name.append("abstract ");
+
+        if (Modifier.isFinal(modifiersNumber))
+            name.append("final ");
+
+        if (Modifier.isInterface(modifiersNumber))
+            name.append("interface ");
+
+        if (Modifier.isPrivate(modifiersNumber))
+            name.append("private ");
+
+        if (Modifier.isNative(modifiersNumber))
+            name.append("native ");
+
+        if (Modifier.isProtected(modifiersNumber))
+            name.append("protected ");
+
+        if (Modifier.isPublic(modifiersNumber))
+            name.append("public ");
+
+        if (Modifier.isStatic(modifiersNumber))
+            name.append("static ");
+
+        if (Modifier.isStrict(modifiersNumber))
+            name.append("strict ");
+
+        if (Modifier.isSynchronized(modifiersNumber))
+            name.append("synchronized ");
+
+        if (Modifier.isTransient(modifiersNumber))
+            name.append("transient ");
+
+        if (Modifier.isVolatile(modifiersNumber))
+            name.append("volatile ");
+
+        if (modifiersNumber == 0)
+            name.append("none");
+
+        return name.toString();
+    }
+}
---
+++ src/com/brainacad/oop/testreflection3/MyClass.java
@@ -0,0 +1,62 @@
+package com.brainacad.oop.testreflection3;
+
+public final class MyClass {
+    private int a;
+    public int b;
+    protected int c;
+
+    public MyClass(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public MyClass() {
+
+    }
+
+    public MyClass(int a, int b, int c, int d) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    int d;
+
+    public MyClass(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+}
